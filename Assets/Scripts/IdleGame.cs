90d77fd54a492e066e405ddf494f73b7fb89026f
VirtRaider <70664238+VirtRaider@users.noreply.github.com>
2020-12-23 22:20:23 -0500

    Working

--- Assets/Scripts/IdleGame.cs
+++ Assets/Scripts/IdleGame.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class IdleGame : MonoBehaviour
 {
     public GameObject upgrades;
+    public GameObject upgrades2;
     public Text coinsText;
     public Text clickValueText;
     public double coins;
@@ -13,39 +14,53 @@ public class IdleGame : MonoBehaviour
     public Text clickUpgrade;
     public Text clickUpgrade2;
     public Text production;
+    public Text production2;
 
     public double coinsPerSecond;
     public double clickUpgradeCost;
     public int clickUpgradeLevel;
 
+    public double clickUpgradeCost2;
+    public int clickUpgradeLevel2;
+
     public double productionUpgradeCost;
     public int productionUpgradeLevel;
 
-    public double clickUpgradeCost2;
-    public int clickUpgradeLevel2;
+    public double productionUpgradeCost2;
+    public double productionUpgradeCost2Power;
+    public int productionUpgradeLevel2;
+
 
     private void Awake()
     {
         upgrades.gameObject.SetActive(false);
+        upgrades2.gameObject.SetActive(false);
     }
 
 
     void Start()
     {
         coinsClickValue = 1;
-        clickUpgradeCost = 10;
-        productionUpgradeCost = 25;
-        clickUpgradeCost2 = 50;
+        clickUpgradeCost = 30;
+        productionUpgradeCost = 30;
+        clickUpgradeCost2 = 150;
+        productionUpgradeCost2 = 150;
+        productionUpgradeCost2Power = 5;
+
     }
 
     
     void Update()
     {
-        if (coins == 10)
+        if (coins >= 30)
         {
             upgrades.gameObject.SetActive(true);
         }
-        coinsPerSecond = productionUpgradeLevel;
+        if (coins >= 150)
+        {
+            upgrades2.gameObject.SetActive(true);
+        }
+        coinsPerSecond = productionUpgradeLevel + (productionUpgradeCost2Power * productionUpgradeLevel2);
 
         clickValueText.text = "Click\n" + coinsClickValue + " Coins";
         coinsText.text = "Coins: " + coins.ToString("F0");
@@ -55,8 +70,9 @@ public class IdleGame : MonoBehaviour
         clickUpgrade2.text = "Click Upgrade 2\nCost: " + clickUpgradeCost2.ToString("F0") + " coins\nPower: +5 Click\nLevel: " + clickUpgradeLevel2;
 
         production.text = "Production Upgrade 1\nCost: " + productionUpgradeCost.ToString("F0") + " coins\nPower: +1 coins/s\nLevel: " + productionUpgradeLevel;
+        production2.text = "Production Upgrade 2\nCost: " + productionUpgradeCost2.ToString("F0") + " coins\nPower: +"+ productionUpgradeCost2Power + " coins/s\nLevel: " + productionUpgradeLevel2;
 
-        coins += coinsPerSecond * Time.deltaTime;
+        coins += coinsPerSecond; //* Time.deltaTime;
     }
 
     // Buttons
@@ -92,6 +108,15 @@ public class IdleGame : MonoBehaviour
             productionUpgradeLevel++;
             coins -= productionUpgradeCost;
             productionUpgradeCost *= 1.07;
+        }
+    }
+    public void BuyProductionUpgrade2()
+    {
+        if (coins >= productionUpgradeCost2)
+        {
+            productionUpgradeLevel2++;
+            coins -= productionUpgradeCost2;
+            productionUpgradeCost2 *= 1.07;
         }
     }
 
